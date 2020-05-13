8e10abef917b50ab62c1066e5e10a69e455fed4e
Max Herold <49107849+maxherold@users.noreply.github.com>
2020-05-13 15:09:47 -0500

    numero-uno
    Initial upload of "Project Mister" software contents

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project_Mister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Project Mister tm");
+        }
+    }
+}
