e6789e8b86a9651607630e3ef748f3553a76e9be
Max Herold <49107849+maxherold@users.noreply.github.com>
2020-05-13 15:35:42 -0500

    number-3
    3

--- Program.cs
+++ Program.cs
@@ -8,10 +8,10 @@ namespace Project_Mister
         {
             Console.WriteLine("Project Mister");
             Console.WriteLine("An epic story of love, loss, and saving the world");
-            Console.WriteLine"Taking place on planet Earth, year 1235. Alternate timeline CPT3");
+            Console.WriteLine("Taking place on planet Earth, year 1235. Alternate timeline CPT-3.");
 
-            Console.Write("This game is all the fuss... probably.");
-            Console.Write("Don't trust my judgment... play it for yourself come Holiday 2023");
+            Console.Write("This game is all the fuss...probably.");
+            Console.Write(" Don't trust my judgment...play it for yourself come Holiday 2023");
         }
     }
 }
